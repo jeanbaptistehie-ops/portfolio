67d1cbb20a971feb003ad8f1248d91c48ee6d16c
Jean-Baptiste Hie <jean-baptiste_hie@ens.univ-artois.fr>
2025-11-12 16:50:55 +0100

    Modification du css de lka navbar et de la homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,12 +2,31 @@ import "../styles/HomePage.css";
 import profile from "../assets/profile.jpg";
 
 export default function HomePage() {
+    const name = "Jean-Baptiste-Hie";
+    const qualities = [
+        "Jovial", "Efficace", "Autonome", "Novateur", "-",
+        "Bienveillant", "Ambitieux", "Ponctuel", "Travailleur", "Investi", "Sérieux",
+        "Tolérant", "Engagé", "-", "Humain", "Ingénieux", "Exigeant"
+    ];
+
     return (
         <section className="hero">
+            <div className="text-container">
+                <h2>
+                    Bonjour, je suis{" "}
+                    <span className="animated-name">
+                        {name.split("").map((letter, index) => (
+                            <span key={index} className="letter" style={{ "--i": index }}>
+                                {letter}
+                                <span className="quality">{qualities[index]}</span>
+                            </span>
+                        ))}
+                    </span>
+                </h2>
+                <br></br>
+                <a href="#projects" className="btn">Voir mes projets</a>
+            </div>
             <img src={profile} alt="photo de profil" className="profile" />
-            <h2>Salut, moi c’est <span>Jean-Baptiste Hie</span></h2>
-            <p>.</p>
-            <a href="#projects" className="btn">Voir mes projets</a>
         </section>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import "./../styles/Navbar.css";
 
 export default function Navbar() {
+    const [open, setOpen] = useState(false);
+
     return (
         <nav className="navbar">
             <h1 className="logo">Mon Portfolio</h1>
-            <ul className="nav-links">
+
+            <ul className={`nav-links ${open ? "open" : ""}`}>
                 <li><a href="#about">À propos</a></li>
                 <li><a href="#projects">Projets</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
+
+            <div className="hamburger" onClick={() => setOpen(!open)}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
         </nav>
     );
 }
